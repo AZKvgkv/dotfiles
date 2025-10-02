f80be50fdfdd40372c68763fe8f4a8f0e98dbc56
AZKvgkv <AZK19991219@outlook.com>
2025-10-02 15:03:27 +0800

    update: 修改颜色切换初始值

--- neovim_configs/nvim/lua/config/keymaps.lua
+++ neovim_configs/nvim/lua/config/keymaps.lua
@@ -49,7 +49,7 @@ keymap("n", "<leader>-", "<CMD>split<CR>", opts)
 keymap("n", "<leader>\\", "<CMD>vsplit<CR>", opts)
 
 -- toggle color between 雷姆蓝 和 拉姆粉
-local color_switch = 1
+local color_switch = 0
 local function toggle_color()
   if color_switch == 1 then
     color_switch = 0
