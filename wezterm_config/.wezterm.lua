edc2474f25a275be139399908f3366ab823692bc
AZKvgkv <AZK19991219@outlook.com>
2026-01-02 17:39:15 +0800

    refactor(config): Update color scheme and keyboard bindings

--- wezterm_config/.wezterm.lua
+++ wezterm_config/.wezterm.lua
@@ -1,7 +1,3 @@
--- TODO: 修改下快捷键--当前与vim的快捷键冲突
---
--- TODO: 改变标签页 让它可以拖动 现在是新建的时候会有 应该保持默认就有
---
 -- Wezterm configuration
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
@@ -14,28 +10,34 @@ local config = wezterm.config_builder()
 -- config.color_scheme = 'CutiePro'
 -- config.color_scheme = "Material Darker (base16)"
 
-function scheme_for_appearance(appearance)
-	if appearance:find("Dark") then
-		return "Catppuccin Mocha" -- or Macchiato, Frappe, Latte
-	else
-		return "Catppuccin Latte"
-	end
-end
+config.color_scheme = 'Darcula (base16)'
 
-color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+
+-- local function scheme_for_appearance(appearance)
+--     return appearance:find("Dark") and "Catppuccin Mocha" or "Catppuccin Latte"
+
+--     -- way 02
+--     -- if appearance:find("Dark") then
+--     --     return "Catppuccin Mocha" -- or Macchiato, Frappe, Latte, Mocha
+--     -- else
+--     --     return "Catppuccin Latte"
+--     -- end
+-- end
+
+-- local color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+-- config.color_scheme = color_scheme
 
 config.window_decorations = "RESIZE"
 config.use_fancy_tab_bar = true
 config.enable_tab_bar = true
 config.show_tab_index_in_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
--- config.tab_bar_at_bottom = true
 config.show_close_tab_button_in_tabs = false
 
 
 config.inactive_pane_hsb = {
-	saturation = 0.9,
-	brightness = 0.8,
+    saturation = 0.9,
+    brightness = 0.8,
 }
 
 -- font
@@ -65,145 +67,130 @@ config.max_fps = 120
 -- }
 
 config.default_prog = {
-	"nu",
+    "nu",
 }
 
+-- todo
 config.window_frame = {
-	font = wezterm.font({
-		family = "Consolas",
-		weight = "Bold",
-	}),
-	font_size = 12.0,
-	active_titlebar_bg = "#090909",
-	inactive_titlebar_bg = "#333333",
+    font = wezterm.font({
+        family = "Consolas",
+        weight = "Bold",
+    }),
+    font_size = 12.0,
+    active_titlebar_bg = "#090909",
+    inactive_titlebar_bg = "#333333",
 }
 config.colors = {
-	tab_bar = {
-		inactive_tab_edge = "#575757",
-	},
+    tab_bar = {
+        inactive_tab_edge = "#575757",
+    },
 }
 
 -------------------- 键盘绑定 --------------------
 local act = wezterm.action
 
--- config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
-config.keys = {
-	{
-		key = "q",
-		mods = "CTRL",
-		action = act.QuitApplication,
-	},
-	{
-		key = "h",
-		mods = "CTRL",
-		action = act.SplitHorizontal({
-			domain = "CurrentPaneDomain",
-		}),
-	},
-	{
-		key = "v",
-		mods = "CTRL",
-		action = act.SplitVertical({
-			domain = "CurrentPaneDomain",
-		}),
-	},
-	{
-		key = "w",
-		mods = "SHIFT|CTRL",
-		action = act.CloseCurrentPane({
-			confirm = true,
-		}),
-	},
-	{
-		key = "LeftArrow",
-		mods = "SHIFT|CTRL",
-		action = act.ActivatePaneDirection("Left"),
-	},
-	{
-		key = "RightArrow",
-		mods = "SHIFT|CTRL",
-		action = act.ActivatePaneDirection("Right"),
-	},
-	{
-		key = "UpArrow",
-		mods = "SHIFT|CTRL",
-		action = act.ActivatePaneDirection("Up"),
-	},
-	{
-		key = "DownArrow",
-		mods = "SHIFT|CTRL",
-		action = act.ActivatePaneDirection("Down"),
-	}, -- CTRL + T 创建默认的Tab
-	{
-		key = "t",
-		mods = "CTRL",
-		action = act.SpawnTab("DefaultDomain"),
-	}, -- CTRL + W 关闭当前Tab
-	{
-		key = "w",
-		mods = "CTRL",
-		action = act.CloseCurrentTab({
-			confirm = false,
-		}),
-	},
+config.keys = config.keys or {}
+-- todo 无法正常显示leader键本身
+config.leader = { key = "\\", mods = "NONE", timeout_milliseconds = 300 }
+local leader_map = {
+    h              = act.SplitVertical { domain = "CurrentPaneDomain" },
+    v              = act.SplitHorizontal { domain = "CurrentPaneDomain" },
+    x              = act.CloseCurrentPane { confirm = true },
+    t              = act.SpawnTab("DefaultDomain"),
+    w              = act.CloseCurrentTab { confirm = false },
+    ["LeftArrow"]  = act.ActivatePaneDirection("Left"),
+    ["RightArrow"] = act.ActivatePaneDirection("Right"),
+    ["UpArrow"]    = act.ActivatePaneDirection("Up"),
+    ["DownArrow"]  = act.ActivatePaneDirection("Down"),
+}
+for k, a in pairs(leader_map) do
+    table.insert(config.keys, { key = k, mods = "LEADER", action = a })
+end
+
+
+local custom_map = {
+    {
+        key = "q",
+        mods = "SHIFT|CTRL",
+        action = act.QuitApplication,
+    },
+    {
+        key = "w",
+        mods = "SHIFT|CTRL",
+        action = act.CloseCurrentPane({
+            confirm = true,
+        }),
+    },
+    {
+        key = "t",
+        mods = "CTRL",
+        action = act.SpawnTab("DefaultDomain"),
+        description = "Create a new tab",
+    },
 }
 
+for _, m in ipairs(custom_map) do
+    table.insert(config.keys, m)
+end
+
+-- todo 这里只写一个 Mod 是可以的
 for i = 1, 8 do
-	-- CTRL + number to activate that tab
-	table.insert(config.keys, {
-		key = tostring(i),
-		mods = "CTRL",
-		action = act.ActivateTab(i - 1),
-	})
+    table.insert(config.keys, {
+        key = tostring(i),
+        mods = "ALT",
+        action = act.ActivateTab(i - 1),
+    })
 end
 
 -------------------- 鼠标绑定 --------------------
-config.mouse_bindings = { -- copy the selection
-	{
-		event = {
-			Up = {
-				streak = 1,
-				button = "Left",
-			},
-		},
-		mods = "NONE",
-		action = act.CompleteSelection("ClipboardAndPrimarySelection"),
-	}, -- Open HyperLink
-	{
-		event = {
-			Up = {
-				streak = 1,
-				button = "Left",
-			},
-		},
-		mods = "CTRL",
-		action = act.OpenLinkAtMouseCursor,
-	},
+config.mouse_bindings            = { -- copy the selection
+    {
+        event = {
+            Up = {
+                streak = 1,
+                button = "Left",
+            },
+        },
+        mods = "NONE",
+        action = act.CompleteSelection("ClipboardAndPrimarySelection"),
+    }, -- Open HyperLink
+    {
+        event = {
+            Up = {
+                streak = 1,
+                button = "Left",
+            },
+        },
+        mods = "CTRL",
+        action = act.OpenLinkAtMouseCursor,
+    },
 }
 
 -------------------- az --------------------
 -- window opacity
--- config.window_background_opacity = 0.85
--- config.win32_system_backdrop     = 'Acrylic'
-
-config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
+config.window_padding            = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
 }
-config.window_background_opacity = 0.85
--- config.win32_system_backdrop     = 'Mica'
+config.window_background_opacity = 0.95
+
+-- Acrylic 最透明，
+-- Mica 磨砂感强，
+-- Tabbed 介于两者之间
+--
+config.win32_system_backdrop     = 'Mica'
 -- config.win32_system_backdrop     = 'Tabbed'
-config.win32_system_backdrop = "Acrylic"
+-- config.win32_system_backdrop     = "Acrylic"
 
 -- windows background image
-local dimmer = {
-	brightness = 0.1,
-}
+-- local dimmer                     = {
+--     brightness = 0.1,
+-- }
 -- config.background = {{
 --     source = {
---         File = 'D:/!Workbenches/10/wezterm-background/voyage.jpg'
+--         File = 'xxxxxx/wezterm-background/voyage.jpg'
 --     },
 --     repeat_x = 'Mirror',
 --     hsb = dimmer,
